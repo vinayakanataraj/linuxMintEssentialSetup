7e6bacf0e5db45ebd885a9117e841fa970ec52a4
vinayakanataraj <86285610+vinayakanataraj@users.noreply.github.com>
2023-09-14 22:18:27 +0530

    Update linuxMintSetup.sh

--- linuxMintSetup.sh
+++ linuxMintSetup.sh
@@ -23,16 +23,7 @@
     cd Tela-circle-icon-theme
     ./install.sh
 
-# brave browser 
-    sudo apt install apt-transport-https curl -y
 
-    sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg -y
-
-    echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-
-    sudo apt update -y
-
-    sudo apt install brave-browser -y
 
 
 # other stuff
